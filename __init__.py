3e1a08232bc2d35259d85072e743de49aec09fba
PrimalFinance <primalfinance101@gmail.com>
2024-09-08 18:20:04 -0700

    Init commit

--- __init__.py
+++ __init__.py
@@ -3,8 +3,4 @@ from laughter_detection import LaughterDetection
 
 if __name__ == "__main__":
 
-    l = LaughterDetection(output_dir="./InEdit")
-    print(l.get_transcription())
-    # r = l.process_video("trump.mp4")
-
-    # r.to_csv("./InEdit/transcript.csv")
+    pass
